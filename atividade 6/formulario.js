937526edee93cbe16cf39bb262c23542d1509a89
Ian <ianguilhermepenha@gmail.com>
2026-06-18 21:43:45 -0300

    Adicionando funções com JS

---
+++ atividade 6/formulario.js
@@ -0,0 +1,27 @@
+function imprimirNome() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let cargo = document.getElementById("cargo").value;
+    let temAcessoRestrito = document.getElementById("temAcessoRestrito").checked;
+
+    console.log("======== DADOS DO CRACHÁ VIRTUAL =======");
+    console.log("Nome (String):", nome);
+    console.log("Idade (Number):", idade)
+    console.log("Cargo (String):", cargo)
+    console.log("Acesso restrito (Boolean):", temAcessoRestrito)
+    console.log("==========================================")
+
+    window.alert(
+        "DADOS DO CRACHÁ\n" +
+        "Nome:" + nome + "\n" +
+        "Idade:" + idade + "\n" +
+        "Cargo: " + cargo + "\n" +
+        "Acesso restrito: " + temAcessoRestrito + "\n"
+    )
+
+    if (temAcessoRestrito) { 
+        window.location.href = "pages/gerente.html"
+    } else {
+        window.location.href = "pages/funcionario.html"
+    }
+}
---
+++ atividade 6/js/cadastroFuncionario.js
@@ -0,0 +1,9 @@
+function cadastrarFuncionario() {
+    let nomeFuncionario = document.getElementById("nome-funcionario").value;
+    let horario = document.getElementById("horas-funcionario").value;
+    let cargo = document.getElementById("cargo-funcionario").value;
+    
+    console.log(nomeFuncionario)
+    console.log(horario)
+    console.log(cargo)
+}
