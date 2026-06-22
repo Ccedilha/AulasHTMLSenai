fbd62edac82babf6b432a42b341a62595446d8e4
Ian <ianguilhermepenha@gmail.com>
2026-06-22 00:49:44 -0300

    feat: adiciona cadastro de funcionarios

--- atividade 6/js/cadastroFuncionario.js
+++ atividade 6/js/cadastroFuncionario.js
@@ -1,9 +1,21 @@
+const funcionarios = [];
 function cadastrarFuncionario() {
-    let nomeFuncionario = document.getElementById("nome-funcionario").value;
-    let horario = document.getElementById("horas-funcionario").value;
-    let cargo = document.getElementById("cargo-funcionario").value;
+    let nomeFuncionario = document.getElementById("nome-funcionario").value.trim();
+    let horario = document.getElementById("horas-funcionario").value.trim();
+    let cargo = document.getElementById("cargo-funcionario").value.trim();
     
-    console.log(nomeFuncionario)
-    console.log(horario)
-    console.log(cargo)
+    if (nomeFuncionario === "" || cargo === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    let funcionarioCadastrado = {
+        nome: nomeFuncionario,
+        horas: horario,
+        cargo: cargo
+    };
+    funcionarios.push(funcionarioCadastrado);
+    localStorage.setItem("funcionarios", JSON.stringify(funcionarios))
+    
+    console.log(funcionarios);
 }
