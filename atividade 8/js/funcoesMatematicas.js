958be099342092cfd4326caa5db5a01f53f53059
Ian <ianguilhermepenha@gmail.com>
2026-06-25 21:37:30 -0300

    feat: atividade 8/9 - fazer uma calculadora de impostos, cujo resultado irá para o LocalStorage

--- atividade 8/js/funcoesMatematicas.js
+++ atividade 8/js/funcoesMatematicas.js
@@ -1,5 +1,5 @@
 const historico = []
-
+const historicoImposto = []
 function soma() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
@@ -36,15 +36,45 @@ function divisao() {
     document.getElementById("resultado").innerText = resultado
     salvarHistorico("Divisão", num1, num2, resultado)
 }
+function impostron2000() {
+    let num3 = document.getElementById("num3").value;
+    let resultadoImposto;
+
+    if (Number(num3) >= 50){
+        resultadoImposto = (Number(num3) * 1.2) * 1.9
+    }else{
+        resultadoImposto = Number(num3) * 1.2
+    }
+
+    document.getElementById("resultadoImposto").innerText = resultadoImposto.toFixed(2)
+    historicoImportacao("calculaImposto", num3, resultadoImposto)
+}
 function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
-    let historicoSalvo = {
+    let historicoCalculos = {
         funcao: nomeDaFuncao,
         numero_1: num1,
         numero_2: num2,
         resultado: resultado
     };
-    historico.push(historicoSalvo);
+    historico.push(historicoCalculos);
     localStorage.setItem("historico", JSON.stringify(historico))
 
     console.log(historico);
+}
+function historicoImportacao(nomeDaFuncao, num3, resultadoImposto){
+        let historicoImportacao = {
+        funcao: nomeDaFuncao,
+        valorInicial: num3,
+        valorFinal: resultadoImposto.toFixed(2)
+    };
+    historicoImposto.push(historicoImportacao);
+    localStorage.setItem("historicoImposto", JSON.stringify(historicoImposto))
+
+    console.log(historicoImposto);
+}
+function apagarLocalStorage(){
+    let confirmaApagar = window.confirm("Você deseja apagar seu histórico?")
+    if(confirmaApagar){
+        localStorage.clear();
+}
 }
