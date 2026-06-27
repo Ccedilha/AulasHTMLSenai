fde62c9b779022a7771fafbb7bf9d6f17007c3dd
Ian <ianguilhermepenha@gmail.com>
2026-06-26 21:43:39 -0300

    atividade para segunda

---
+++ atividade 9/app/buscaCEP.js
@@ -0,0 +1,24 @@
+function buscaCEP(){
+    let cepDigitado = document.getElementById("cep").value;
+
+    if (cepDigitado === ""){
+        return;
+    }
+    fetch("https://viacep.com.br/ws/${cepDigitado}/json/")
+    then(function(resposta){
+            return resposta.json();
+    })
+    then(function(dados){
+        if(dados.erro){
+            alert("CEP não encontrado")
+            return;
+        }
+        document.getElementById("rua").value = dados.logradouro;
+        document.getElementById("bairro").value = dados.bairro;
+        document.getElementById("cidade").value = dados.localidade;
+        document.getElementById("uf").value = dados.uf;
+    })
+}
+
+const campoCEP = document.getElementById("cep");
+campoCEP.addEventListener('focusout', buscaCEP);
